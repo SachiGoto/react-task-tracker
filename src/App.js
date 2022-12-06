98c8855b0ceefc5be0497eb2ad33cce7a68d2cfb
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2022-12-05 18:29:40 -0800

    added tasks.js, button.js, header.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id:3,
+        text:"Shopping"
+    },
+    {
+        id:5,
+        text:"go watch a movie"
+    },
+    {
+        id:10,
+        text:"do codewars"
+    }
+])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       {/* <Header title = "Hello"/> */}
+       <Header/>
+       <Tasks tasks={tasks} />
     </div>
   );
 }
+
+
+// class App extends React.Component{
+//     render(){
+//       return <h1>Hello from a class</h1>
+//     }
+// }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+
+
+
+
+
+const Header = ({title}) => {
+
+ const onClick = () =>{
+    console.log('Clicked')
+ }
+
+  return (
+    <header className='header'>
+      {/* <h1 style={headingStyle}>{title}</h1> */}
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
+    
+     
+      
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title:'Task Tracker',
+}
+
+
+Header.propTypes = {
+    title:PropTypes.string.isRequired
+
+}
+
+//css in js
+
+// const headingStyle = {
+//    color:'red', 
+//    backgroundColor:'black'
+
+
+// }
+
+export default Header
+
